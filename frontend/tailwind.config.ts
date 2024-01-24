8cac57efe46b021147ab929179e1dfe502fe3ca9
Jarosal <azethis@gmail.com>
2024-01-24 19:51:12 +0100

    working skeleton start page

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,16 +1,13 @@
 import { join } from 'path';
-import type { Config } from 'tailwindcss';
-
 import { skeleton } from '@skeletonlabs/tw-plugin';
+
+import type { Config } from 'tailwindcss';
 
 const config = {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
+		join(require.resolve('@skeletonlabs/skeleton'),'../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
 		extend: {},
