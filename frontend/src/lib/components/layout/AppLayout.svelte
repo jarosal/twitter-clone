506a897a7a4e4989909709e10d96e3c1daef7b1d
Jarosal <azethis@gmail.com>
2024-02-15 21:55:48 +0100

    init layout

---
+++ frontend/src/lib/components/layout/AppLayout.svelte
@@ -0,0 +1,21 @@
+<!-- container -->
+<div class="container mx-auto h-screen">
+    <!-- left sidebar -->
+    <div class="w-68 xs:w-88 xl:w-275 h-screen">
+        <div class="flex flex-col h-screen xl:pr-3 fixed overflow-y-auto w-68 xs:w-88 xl:w-275">
+
+        </div>
+    </div>
+
+    <!-- content -->
+    <div class="w-full sm:w-600 h-screen">
+        <slot />
+    </div>
+
+    <!-- right sidebar -->
+    <div class="hidden md:block w-290 lg:w-350 h-screen">
+        <div class="flex flex-col fixed overflow-y-auto w-290 lg:w-350 h-screen">
+            
+        </div>
+    </div>
+</div>
