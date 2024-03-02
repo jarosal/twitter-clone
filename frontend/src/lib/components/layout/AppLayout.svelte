6aad0bcf5f9b8c0b4a6e51e569f22503769d698b
Jarosal <azethis@gmail.com>
2024-03-02 12:19:22 +0100

    base layout

--- frontend/src/lib/components/layout/AppLayout.svelte
+++ frontend/src/lib/components/layout/AppLayout.svelte
@@ -1,21 +1,51 @@
 <!-- container -->
-<div class="container mx-auto h-screen">
-    <!-- left sidebar -->
-    <div class="w-68 xs:w-88 xl:w-275 h-screen">
-        <div class="flex flex-col h-screen xl:pr-3 fixed overflow-y-auto w-68 xs:w-88 xl:w-275">
+<div id="app-layout" class="container mx-auto h-screen">
+	<div class="flex flex-row justify-center">
+		<!-- left sidebar -->
+		<div id="left-sidebar" class="w-68 xs:w-88 xl:w-275 h-screen">
+			<div class="w-68 xs:w-88 xl:w-275 fixed flex h-screen flex-col overflow-y-auto pr-8">
+				{#if $$slots.headerLeft}
+					<slot name="headerLeft" />
+				{/if}
 
-        </div>
-    </div>
+				{#if $$slots.sidebarLeft}
+					<slot name="sidebarLeft" />
+				{/if}
 
-    <!-- content -->
-    <div class="w-full sm:w-600 h-screen">
-        <slot />
-    </div>
+				{#if $$slots.footerLeft}
+					<slot name="footerLeft" />
+				{/if}
+			</div>
+		</div>
 
-    <!-- right sidebar -->
-    <div class="hidden md:block w-290 lg:w-350 h-screen">
-        <div class="flex flex-col fixed overflow-y-auto w-290 lg:w-350 h-screen">
-            
-        </div>
-    </div>
+		<!-- content -->
+		<div id="content" class="sm:w-600 h-screen w-full bg-slate-400">
+			{#if $$slots.contentHeader}
+				<slot name="contentHeader" />
+			{/if}
+
+			<main id="page-content"><slot /></main>
+
+			{#if $$slots.contentFooter}
+				<slot name="contentFooter" />
+			{/if}
+		</div>
+
+		<!-- right sidebar -->
+		<div id="right-sidebar" class="w-290 lg:w-350 hidden h-screen md:block">
+			<div class="w-290 lg:w-350 fixed flex h-screen flex-col overflow-y-auto pl-8">
+				{#if $$slots.headerRight}
+					<slot name="headerRight" />
+				{/if}
+
+				{#if $$slots.sidebarRight}
+					<slot name="sidebarRight" />
+				{/if}
+
+				{#if $$slots.footerRight}
+					<slot name="footerRight" />
+				{/if}
+			</div>
+		</div>
+	</div>
 </div>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.pcss';
+
+	import AppLayout from '$lib/components/layout/AppLayout.svelte';
+</script>
+
+<AppLayout>
+	<slot />
+</AppLayout>
