d901ecf3daacbd7bd150bb930d59ef5d70229ac7
jarosal <52526948+jarosal@users.noreply.github.com>
2024-03-09 11:40:59 +0100

    Add icons and navigation (#7)

--- frontend/src/lib/components/layout/AppLayout.svelte
+++ frontend/src/lib/components/layout/AppLayout.svelte
@@ -1,9 +1,14 @@
-<!-- container -->
-<div id="app-layout" class="container mx-auto h-screen">
-	<div class="flex flex-row justify-center">
-		<!-- left sidebar -->
-		<div id="left-sidebar" class="w-68 xs:w-88 xl:w-275 h-screen">
-			<div class="w-68 xs:w-88 xl:w-275 fixed flex h-screen flex-col overflow-y-auto pr-8">
+<!-- App container -->
+<div id="app-layout" class="flex w-full justify-center gap-0 lg:gap-4">
+	<!-- left sidebar -->
+	<header
+		id="left-sidebar"
+		class="xs:w-20 flex w-0 shrink-0 transition-opacity duration-200 md:w-24 lg:max-w-none xl:-mr-4 xl:w-full xl:max-w-xs xl:justify-end"
+	>
+		<div
+			class="border-bg-accent xs:top-0 xs:h-full xs:w-auto xs:border-0 xs:bg-transparent xs:px-2 xs:py-3 xs:pt-2 fixed bottom-0 z-10 flex w-full flex-col justify-between border-t py-0 md:px-4 xl:w-72"
+		>
+			<div class="xs:items-center flex flex-col justify-center gap-2 xl:items-stretch">
 				{#if $$slots.headerLeft}
 					<slot name="headerLeft" />
 				{/if}
@@ -11,41 +16,41 @@
 				{#if $$slots.sidebarLeft}
 					<slot name="sidebarLeft" />
 				{/if}
-
-				{#if $$slots.footerLeft}
-					<slot name="footerLeft" />
-				{/if}
 			</div>
-		</div>
 
-		<!-- content -->
-		<div id="content" class="sm:w-600 h-screen w-full bg-slate-400">
-			{#if $$slots.contentHeader}
-				<slot name="contentHeader" />
-			{/if}
-
-			<main id="page-content"><slot /></main>
-
-			{#if $$slots.contentFooter}
-				<slot name="contentFooter" />
+			{#if $$slots.footerLeft}
+				<slot name="footerLeft" />
 			{/if}
 		</div>
+	</header>
 
-		<!-- right sidebar -->
-		<div id="right-sidebar" class="w-290 lg:w-350 hidden h-screen md:block">
-			<div class="w-290 lg:w-350 fixed flex h-screen flex-col overflow-y-auto pl-8">
-				{#if $$slots.headerRight}
-					<slot name="headerRight" />
-				{/if}
+	<!-- Page content -->
+	<div id="content" class="h-screen w-full bg-slate-400 sm:w-600">
+		{#if $$slots.contentHeader}
+			<slot name="contentHeader" />
+		{/if}
 
-				{#if $$slots.sidebarRight}
-					<slot name="sidebarRight" />
-				{/if}
+		<main id="page-content"><slot /></main>
 
-				{#if $$slots.footerRight}
-					<slot name="footerRight" />
-				{/if}
-			</div>
+		{#if $$slots.contentFooter}
+			<slot name="contentFooter" />
+		{/if}
+	</div>
+
+	<!-- Right sidebar -->
+	<div id="right-sidebar" class="hidden h-screen w-290 md:block lg:w-350">
+		<div class="fixed flex h-screen w-290 flex-col overflow-y-auto pl-8 lg:w-350">
+			{#if $$slots.headerRight}
+				<slot name="headerRight" />
+			{/if}
+
+			{#if $$slots.sidebarRight}
+				<slot name="sidebarRight" />
+			{/if}
+
+			{#if $$slots.footerRight}
+				<slot name="footerRight" />
+			{/if}
 		</div>
 	</div>
 </div>
