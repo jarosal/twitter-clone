6a6fa4e03a12c2925dd157a36a4e909700a5aca4
Jarosal <azethis@gmail.com>
2024-03-12 21:31:57 +0100

    Initial profile component

--- frontend/src/lib/components/layout/AppLayout.svelte
+++ frontend/src/lib/components/layout/AppLayout.svelte
@@ -3,10 +3,10 @@
 	<!-- left sidebar -->
 	<header
 		id="left-sidebar"
-		class="xs:w-20 flex w-0 shrink-0 transition-opacity duration-200 md:w-24 lg:max-w-none xl:-mr-4 xl:w-full xl:max-w-xs xl:justify-end"
+		class="flex w-0 shrink-0 transition-opacity duration-200 xs:w-20 md:w-24 lg:max-w-none xl:-mr-4 xl:w-full xl:max-w-xs xl:justify-end"
 	>
 		<div
-			class="border-bg-accent xs:top-0 xs:h-full xs:w-auto xs:border-0 xs:bg-transparent xs:px-2 xs:py-3 xs:pt-2 fixed bottom-0 z-10 flex w-full flex-col justify-between border-t py-0 md:px-4 xl:w-72"
+			class="border-bg-accent xs:top-0 xs:h-full xs:w-auto xs:border-0 xs:bg-transparent px-2 py-3 xs:pt-2 bottom-0 z-10 flex w-full flex-col justify-between border-t  md:px-4 xl:w-72"
 		>
 			<div class="xs:items-center flex flex-col justify-center gap-2 xl:items-stretch">
 				{#if $$slots.headerLeft}
