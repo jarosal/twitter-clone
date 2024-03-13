0e2bea8a1be21b335bbf65c84f7bae23ea62d307
jarosal <azethis@gmail.com>
2024-03-13 11:43:44 +0100

    Hover improvements

--- frontend/src/lib/components/layout/SidebarProfile.svelte
+++ frontend/src/lib/components/layout/SidebarProfile.svelte
@@ -10,12 +10,12 @@
 
 <div class="relative">
 	<button
-		class="hover:bg-accent flex w-full items-center justify-between rounded-full p-3 transition duration-300 focus-visible:ring-2 focus-visible:ring-accent-foreground outline-none"
+		class="group hover:bg-accent flex w-full items-center justify-between rounded-full p-3 transition duration-300 focus-visible:ring-2 focus-visible:ring-accent-foreground outline-none"
 	>
 		<div class="flex gap-3 truncate">
 			<Avatar.Root>
 				<Avatar.Image alt="Lorem Ipsum"/>
-				<Avatar.Fallback>LI</Avatar.Fallback>
+				<Avatar.Fallback class="group-hover:bg-white transition duration-300">LI</Avatar.Fallback>
 			</Avatar.Root>
 
 			<div class="hidden truncate text-start leading-5 xl:block">
