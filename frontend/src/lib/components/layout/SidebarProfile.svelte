4d0dca122f7cc25a53bdf946353f10f3169cedad
jarosal <azethis@gmail.com>
2024-03-13 08:16:23 +0100

    Use avatar component

--- frontend/src/lib/components/layout/SidebarProfile.svelte
+++ frontend/src/lib/components/layout/SidebarProfile.svelte
@@ -1,51 +1,29 @@
 <script lang="ts">
-	import RiMoreLine from 'virtual:icons/ri/more-line';
+	import Dots from 'virtual:icons/ri/more-line';
+	import * as Avatar from "$lib/components/ui/avatar";
 </script>
+
+<!--
+	- extract props
+	- context menu
+-->
 
 <div class="relative">
 	<button
-		class="hover:bg-accent flex w-full items-center justify-between rounded-full p-3"
+		class="hover:bg-accent flex w-full items-center justify-between rounded-full p-3 transition duration-300 focus-visible:ring-2 focus-visible:ring-accent-foreground outline-none"
 	>
 		<div class="flex gap-3 truncate">
-			<img
-				class="h-10 w-10 rounded-full"
-				src="https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_normal.jpg"
-				alt="Joe Biden"
-			/>
+			<Avatar.Root>
+				<Avatar.Image alt="Lorem Ipsum"/>
+				<Avatar.Fallback>LI</Avatar.Fallback>
+			</Avatar.Root>
 
 			<div class="hidden truncate text-start leading-5 xl:block">
-				<h1 class="text-sm font-bold text-gray-800 dark:text-white">Joe Biden</h1>
-				<p class="text-sm text-gray-400">@JoeBiden</p>
+				<h1 class="text-sm font-bold text-gray-800 dark:text-white">Lorem ipsum</h1>
+				<p class="text-sm text-gray-400">@LoremIpsum</p>
 			</div>
 			
 		</div>
-    <RiMoreLine class="hidden h-5 w-5 xl:block" />
+    <Dots class="hidden h-5 w-5 xl:block" />
 	</button>
 </div>
-
-<!-- <div
-	class="dark:hover:bg-dim-800 duration-350 mx-auto mb-5 mt-auto flex w-14 cursor-pointer flex-row justify-between rounded-full p-2 transition ease-in-out hover:bg-blue-50 xl:w-full"
->
-	<div class="flex flex-row">
-		<img
-			class="h-10 w-10 rounded-full"
-			src="https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_normal.jpg"
-			alt="Joe Biden"
-		/>
-		<div class="ml-2 flex hidden flex-col xl:block">
-			<h1 class="text-sm font-bold text-gray-800 dark:text-white">Joe Biden</h1>
-			<p class="text-sm text-gray-400">@JoeBiden</p>
-		</div>
-	</div>
-	<div class="hidden xl:block">
-		<div class="flex h-full items-center text-gray-800 dark:text-white">
-			<svg viewBox="0 0 24 24" fill="currentColor" class="mr-2 h-4 w-4">
-				<g>
-					<path
-						d="M20.207 8.147c-.39-.39-1.023-.39-1.414 0L12 14.94 5.207 8.147c-.39-.39-1.023-.39-1.414 0-.39.39-.39 1.023 0 1.414l7.5 7.5c.195.196.45.294.707.294s.512-.098.707-.293l7.5-7.5c.39-.39.39-1.022 0-1.413z"
-					></path>
-				</g>
-			</svg>
-		</div>
-	</div>
-</div> -->
