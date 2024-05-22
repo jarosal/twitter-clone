fa9a4dd95ae3a645b4948a555ca1fa9ca94ea079
Jarosal <azethis@gmail.com>
2024-05-22 23:24:06 +0200

    add GraphQL to HelloWorld

--- backend/src/TwitterClone.HelloWorld/Program.cs
+++ backend/src/TwitterClone.HelloWorld/Program.cs
@@ -2,10 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Services
+  .AddGraphQLServer();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapDefaultEndpoints();
+
+app.MapGraphQL();
 
 app.Run();
