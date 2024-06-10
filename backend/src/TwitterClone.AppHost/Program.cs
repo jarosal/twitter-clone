2598828100de060c81e5dfed8302e4928e4b6743
Jarosal <azethis@gmail.com>
2024-06-10 20:33:00 +0200

    reconfigure Aspire

--- backend/src/TwitterClone.AppHost/Program.cs
+++ backend/src/TwitterClone.AppHost/Program.cs
@@ -1,20 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var frontend = builder
-  .AddNpmApp("twitter-clone-frontend", "../../../frontend", "aspire")
-  .WithHttpEndpoint(env: "PORT")
-  .WithExternalHttpEndpoints()
-  .PublishAsDockerFile();
-
 var helloWorldApi = builder
   .AddProject<Projects.TwitterClone_HelloWorld>("hello-world");
 
-frontend
-  .WithEnvironment("HELLO_WORLD_API", helloWorldApi.GetEndpoint("http"));
-
-builder
+var graphQLGateway = builder
   .AddFusionGateway<Projects.TwitterClone_GraphQLGateway>("graphql-gateway")
   .WithSubgraph(helloWorldApi);
+
+builder
+  .AddNpmApp("twitter-clone-frontend", "../../../frontend", "aspire")
+  .WithHttpEndpoint(env: "PORT")
+  .WithExternalHttpEndpoints()
+  .PublishAsDockerFile()
+  .WithEnvironment("PUBLIC_GRAPHQL_GATEWAY", "http://localhost:5098/graphql");
 
 builder
   .Build()
