b8222b5a9a7297c4795ff10352093db46228aa94
Jarosal <azethis@gmail.com>
2024-06-12 22:54:04 +0200

    cleanup

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -2,7 +2,7 @@ import { HoudiniClient } from '$houdini';
 import { env } from '$env/dynamic/public';
 
 export default new HoudiniClient({
-    url: env.PUBLIC_GRAPHQL_GATEWAY ?? ''
+    url: env.PUBLIC_GRAPHQL_GATEWAY
 
     // uncomment this to configure the network call (for things like authentication)
     // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
